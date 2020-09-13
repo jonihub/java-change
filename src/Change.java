f4820f0875d73fe6717e687d787f135e30690f16
Jonathan Cooper <jonnymusicguy@gmail.com>
2020-09-13 17:49:59 -0400

    Modified Change

--- src/Change.java
+++ src/Change.java
@@ -7,67 +7,46 @@ import java.util.Scanner;
 
 public class Change {
     public static void main(String[] args) {
+
+        int[] coins = {10000, 5000, 2000, 1000, 500, 100, 50, 25, 10, 5, 1};
         
         // Set up to recieve user input
         System.out.print("Please enter amount of money (ex. 18.07): $");
         Scanner sc = new Scanner(System.in);
         double money = sc.nextDouble();
-
+       
         // Convert user input to cents
         int value = (int) (money * 100);
-    
-        // Check number of $100s
-        int hundreds = value / 10000;
-        value = value % 10000;
-
-        // Check number of $20s
-        int twenties = value / 2000;
-        value = value % 2000;
-
-        // Check number of $10s
-        int tens = value / 1000;
-        value = value % 1000;
-
-        // Check number of $5s
-        int fives = value / 500;
-        value = value % 500;
-
-        // Check number of $1s
-        int ones = value / 100;
-        value = value % 100;
-        
-        // Check number of 25¢s
-        int quarters = value / 25;
-        value = value % 25;
-
-        // Check number of 10¢s
-        int dimes = value / 10;
-        value = value % 10;
-
-        // Check number of 5¢s
-        int nickel = value / 5;
-        value = value % 5;
-
-        // Check number of 1¢s
-        int penny = value;
+        // Determine how many of each denomination are required
+        int[] count = coin_calc(coins, value);
 
         // Display the results
-        String output = "Your amount $" + money + " consists of: \n" + 
-        "\t" + hundreds + " hundred dollar(s)\n" +
-        "\t" + twenties + " twenty dollar(s)\n" +
-        "\t" + tens + " ten dollar(s)\n" +
-        "\t" + fives + " five dollar(s)\n" +
-        "\t" + ones + " one dollar(s)\n" +
-        "\t" + quarters + " quarter(s)\n" +
-        "\t" + dimes + " dime(s)\n" +
-        "\t"+ nickel + " nickel(s)\n" +
-        "\t" + penny + " penny(ies)";
+        System.out.println("Your amount $" + money + " consists of " + value + " coin(s).");
+        for (int i = 0; i < coins.length; i++) {
+            if (coins[i] >= 100) {
+                System.out.println("\t" + count[i] + " - " + coins[i]/100 + " dollar(s)");
+            }
+            else {
+                System.out.println("\t" + count[i] + " - " + coins[i] + " cent(s)");
+            }
+         }
 
-        if (money != 0) {
-            System.out.println(output);
-        }
-        else{
-            System.out.println("You didn't put any money");
-        }
+         sc.close();
     }
+
+    static int[] coin_calc(int[] coins, int amount) {
+        
+        int[] count = new int[coins.length];
+        // Loop through each denomination (starting at largest)
+        for (int i = 0; i < coins.length; i++) {
+            
+            // Use one of that denomination until we need something smaller
+            while (amount >= coins[i]) {
+                count[i]++;
+                amount -= coins[i];
+            }
+        }
+        return count;
+    }
+
 }
